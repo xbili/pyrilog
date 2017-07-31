0c660c8f497eb6659031c8753e502c64fc4236ec
Bili Xu <xubili93@gmail.com>
2017-07-31 22:08:39 +0800

    Add stubbed generate_verilog method

---
+++ pyrilog/generate_verilog.py
@@ -0,0 +1,6 @@
+def generate_verilog(wires, entities):
+    """
+    Given the wires and entities of a hardware design, this method generates
+    the Verilog code that is synthesizable.
+    """
+    pass
