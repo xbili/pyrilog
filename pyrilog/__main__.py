a1ddab3fd80b9df5576d4fec535a6a8e9f64ac54
Bili Xu <xubili93@gmail.com>
2017-08-01 00:12:48 +0800

    Add CLI interface for usage

--- pyrilog/__main__.py
+++ pyrilog/__main__.py
@@ -1,1 +1,42 @@
+import click
 
+from pyrilog.core.wire import Wire
+from pyrilog.multiplier import create
+from pyrilog.verilog import generate
+
+
+@click.command()
+@click.option(
+    '--size',
+    default=4,
+    help='size of the vectors that we want to perform dot product operations on.',
+)
+@click.option(
+    '--width',
+    default=4,
+    help='bit width of each element in the vectors that we want to perform dot product on.'
+)
+@click.option(
+    '--output-dir',
+    default='./generated.v',
+    help='the output directory of your generated Verilog file, defaults to your current directory.'
+)
+@click.option(
+    '--with-pipeline',
+    is_flag=True,
+    help="we'll include latches in between the stages of the partial product reduction tree if this option is checked."
+)
+def main(size, width, output_dir, with_pipeline):
+    print('WARNING: Current implementation only includes a multiplier')
+
+    multiplier, multiplicand = [Wire() for _ in range(width)], [Wire() for _ in range(width)]
+    output, entities = create(multiplier, multiplicand, width)
+    generated = generate([multiplier, multiplicand], [output], entities)
+
+    with open(output_dir, 'w') as f:
+        f.write(generated)
+
+    print('Success! Generated Verilog file saved at {}'.format(output_dir))
+
+if __name__ == '__main__':
+    main()
